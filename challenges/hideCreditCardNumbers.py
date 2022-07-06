875f0424fa675ca8a45473272ed4ac41ab5d52a2
Lucas Fernando <lucas98fernando@gmail.com>
2022-07-06 13:34:50 -0300

    Vowels count and hide credit card numbers challenges

---
+++ challenges/hideCreditCardNumbers.py
@@ -0,0 +1,4 @@
+creditCard = input(
+    'Informe os números do cartão de crédito(13 a 16 caracteres): ')
+
+print(f'Últimos 4 dígitos do cartão: {creditCard[-4:]}')
---
+++ challenges/vowelsCount.py
@@ -0,0 +1,13 @@
+print('Contador de vogais\n')
+
+word = input('Informe uma palavra: ')
+vowels = ['a', 'e', 'i', 'o', 'u']
+vowelsCount = 0
+
+if word:
+    for letter in word:
+        if (letter in vowels):
+            vowelsCount += 1
+    print(f'A quantidade de vogais na palavra {word} é {vowelsCount}')
+else:
+    print('Informe uma palavra')
