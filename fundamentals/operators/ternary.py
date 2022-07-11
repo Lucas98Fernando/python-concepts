c24a9d1e33952aba99c03c5a6f8420fa7d8353b2
Lucas Fernando <lucas98fernando@gmail.com>
2022-07-11 00:21:32 -0300

    Another way to use ternary operators

--- fundamentals/operators/ternary.py
+++ fundamentals/operators/ternary.py
@@ -1,5 +1,10 @@
 isPythonCool = True
 
+# Utilizando IF | ELSE
 status = 'Python é muito massa!' if isPythonCool else 'Você tá mentindo'
 
 print(status)
+
+# Outra forma de utilizar operadores ternários
+isRaining = True
+msg = 'Hoje estou com as roupas ' + ('secas.', 'molhadas')[isRaining]
